69fdee9c5b50173205ac2800c0c301f45af693e2
Dhanya Sridhar <dhanyasridhar@eduroam-241-248.ucsc.edu>
2015-02-12 13:07:16 -0800

    modifications to tester.py to test edge iteration count

--- code/tester.py
+++ code/tester.py
@@ -4,7 +4,7 @@ import triangle_counters
 print('Creating empty graph G')
 G = graph_tools.graph()                      # Create empty graph
 
-fname = '../graphs/amazon0312.txt'           # Enter file name
+fname = '../graphs/facebook_combined.txt'           # Enter file name
 print('Reading edges from file',fname,'into G')
 G.Read_edges(fname)   # Read edges from file (in this case it is from amazon0312.txt)
 
@@ -21,8 +21,11 @@ print('Size of G: vertices = ',G_size[0],', sum of degrees = ',G_size[1],', wedg
 D_size = D.Size()         # Output size of D
 print('Size of D: vertices = ',D_size[0],', sum of degrees = ',D_size[1],', wedges =',D_size[2])
 
-dg_count = triangle_counters.wedge_enum(D)       # Perform wedge enumeration on D to count triangles
-print('Running wedge enumeration on D, triangle count =',dg_count)
+# dg_count = triangle_counters.wedge_enum(D)       # Perform wedge enumeration on D to count triangles
+# print('Running wedge enumeration on D, triangle count =',dg_count)
 
 g_count = triangle_counters.wedge_enum(G)        # Perform wedge enumeration on G to count triangles. Note that this is three times the number of triangles.
 print('Running wedge enumeration on G, triangle count =',g_count)
+
+g_count_edge_iter = triangle_counters.edge_iter(G)
+print('Running edge iteration on G, triangle count =', g_count_edge_iter)
