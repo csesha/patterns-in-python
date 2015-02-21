4b8c1ddba9595f02f0861515ee663d9c4dd540ca
csesha <csesha@gmail.com>
2015-02-20 21:27:57 -0800

    Used floats for triangle counts

--- code/triangle_counters.py
+++ code/triangle_counters.py
@@ -13,13 +13,13 @@ import sys
 #### 
 
 def wedge_enum(G, wedges=False):
-    wedge_cnt = 0
-    closed = 0              # Initialize number of closed wedges
+    wedge_cnt = 0.0
+    closed = 0.0              # Initialize number of closed wedges
     for node1 in G.vertices:     # Loop over all nodes
         for (node2, node3) in itertools.combinations(G.adj_list[node1],2):    #Loop over all pairs of neighbors of node1
             wedge_cnt += 1
             if G.isEdge(node2,node3):    # If (node2, node3) form an edge
-                closed += 1              # Then wedge participating in trianglehas been found!
+                closed += 1.0              # Then wedge participating in trianglehas been found!
     if wedges:
       return (closed, wedge_cnt)
     return closed
@@ -36,10 +36,10 @@ def triangle_info(DAG):
     tri_edge = {}
 
     for node in DAG.vertices:
-        tri_vertex[node] = 0   # Initialize for each vertex
+        tri_vertex[node] = 0.0   # Initialize for each vertex
         for nbr in DAG.adj_list[node]:
-            tri_edge[(node,nbr)] = 0    # Initialize for each edge, note each edge is tuple with lower vertex first
-            tri_edge[(nbr,node)] = 0    # Initialize for each edge, note each edge is tuple with lower vertex first
+            tri_edge[(node,nbr)] = 0.0    # Initialize for each edge, note each edge is tuple with lower vertex first
+            tri_edge[(nbr,node)] = 0.0    # Initialize for each edge, note each edge is tuple with lower vertex first
 
     for node1 in DAG.vertices:     # Loop over all nodes
         for (node2, node3) in itertools.combinations(DAG.adj_list[node1],2):    #Loop over all pairs of neighbors of node1
